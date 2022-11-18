8ba02ad065f675c60a0f182d0b054fe67d9ff62c
이강윤 <lky61010@naver.com>
2022-11-18 10:17:27 +0900

    B REFACTOR 006 : 로그인 방식 변경

--- final-pjt-back/DRF_server/accounts/models.py
+++ final-pjt-back/DRF_server/accounts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     # followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
