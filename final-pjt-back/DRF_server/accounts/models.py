2546b3222bf9e48c1cd19062671306b8731d2051
이강윤 <lky61010@naver.com>
2022-11-18 13:22:09 +0900

    B REFACTOR 006 : 유저의 선호 장르, 배우, 영화 추출 완료

--- final-pjt-back/DRF_server/accounts/models.py
+++ final-pjt-back/DRF_server/accounts/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- final-pjt-back/DRF_server/movies/urls.py
+++ final-pjt-back/DRF_server/movies/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('actors/', views.actor_list),
     path('like_genres/', views.genre_index, name='like_genres'),
     path('genres/<int:genre_pk>/like/', views.like_genre),
+    path('actors/<int:actor_pk>/like/', views.like_actor),
+    path('<int:movie_pk>/like/', views.like_movie),
     path('user_likes/', views.user_likes, name='user_likes'),
     # path('reviews/', views.review_list),
     # path('reviews/<int:review_pk>/', views.review_detail),
