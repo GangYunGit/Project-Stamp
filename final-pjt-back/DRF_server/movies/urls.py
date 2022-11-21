4ba9c0c688ed8c5966432ee8101bd94998aa2f55
이강윤 <lky61010@naver.com>
2022-11-21 17:48:38 +0900

    B CHORE : swagger 페이지 추가

--- final-pjt-back/DRF_server/movies/urls.py
+++ final-pjt-back/DRF_server/movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
-app_name='movies'
+# swagger 사용하려면 app_name을 잠깐 주석처리하고 사용!
+app_name = 'movies'
 
 urlpatterns = [
-
     path('', views.movie_list),
     path('<int:movie_pk>/', views.movie_detail),
     path('genres/', views.genre_list),
@@ -16,5 +17,10 @@ urlpatterns = [
     path('user_likes/', views.user_likes, name='user_likes'),
     path('users/', views.users),
     path('recommendation/<int:user_pk>/', views.recommendation),
-
+    # 필수 작성
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # optional UI
+    path(
+        'swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'
+    ),
 ]
--- final-pjt-back/DRF_server/mypjt/settings.py
+++ final-pjt-back/DRF_server/mypjt/settings.py
@@ -77,11 +77,19 @@ REST_FRAMEWORK = {
     ],
     # permission
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
     ],
     # spectacular Settings
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
 
 MIDDLEWARE = [
